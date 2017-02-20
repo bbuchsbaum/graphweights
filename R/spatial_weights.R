6af06581c3a2c94301b60756daad938dd8c3a250
bbuchsbaum <brad.buchsbaum@gmail.com>
2017-02-20 17:36:04 -0500

    yolo

--- R/spatial_weights.R
+++ R/spatial_weights.R
@@ -8,24 +8,212 @@ spatial_laplacian <- function(coord_mat, dthresh=1.42, nnk=27) {
 }
 
 
-#' spatial_adjacency
-#'
-#' @importFrom FNN get.knnx
-#' @importFrom Matrix sparseMatrix
-#' @export
-spatial_adjacency <- function(coord_mat, dthresh=1.42, nnk=27) {
-  full_nn <- FNN::get.knnx(coord_mat, coord_mat, nnk)
+make_doubly_stochastic <- function(sm, tol=nrow(sm) * .01) {
+  e <- 100000
 
-  triplet <- do.call(rbind, lapply(1:nrow(coord_mat), function(i) {
-    dvals <- full_nn$nn.dist[i,1:nnk]
-    keep <- dvals > 0 & dvals < dthresh
-    if (keep[2]) {
-      cbind(i,full_nn$nn.index[i,keep], 1)
-    } else {
-      NULL
-    }
-  }))
+  while (e > tol) {
 
-  sparseMatrix(i=triplet[,1], j=triplet[,2], x=triplet[,3])
+    D <- rowSums(sm)
+    D1 <- 1/sqrt(D)
+    sm <- D1 * sm * D1
+    sm <- (sm + t(sm))/2
+    e <- sum(abs(rowSums(sm) -1) + abs(colSums(sm) -1))
+    print(paste("e = ", e))
+  }
+  sm
 
 }
+
+normalize_adjacency <- function(sm, symmetric=TRUE) {
+  D <- rowSums(sm)
+
+  D1 <- 1/sqrt(D)
+  sm <- D1 * sm * D1
+
+  if (symmetric) {
+    sm <- (sm + t(sm))/2
+  }
+
+  sm
+}
+
+#' cross_weighted_spatial_adjacency
+#'
+#' @param coord_mat1 the first coordinate matrix (the query)
+#' @param coord_mat2 the second coordinate matrix (the reference)
+#' @param feature_mat1 the first feature matrix
+#' @param feature_mat2 the second feature matrix
+#' @param wsigma the sigma for the feature kernel
+#' @param alpha the weightinf or the spatial distance (1=all spatial, 0=all feature)
+#' @param dthresh the threshold for the spatial distance
+#' @param normalized whether to normalize the rows to sum to 1
+#' @importFrom assertthat assert_that
+cross_weighted_spatial_adjacency <- function(coord_mat1, coord_mat2,
+                                             feature_mat1, feature_mat2,
+                                             wsigma=.73, alpha=.5,
+                                             nnk=27,
+                                             weight_mode=c("binary", "heat"),
+                                             sigma=1,
+                                             dthresh=sigma*2.5,
+                                             normalized=TRUE) {
+
+
+  assert_that(nrow(feature_mat1) == nrow(coord_mat1))
+  assert_that(nrow(feature_mat2) == nrow(coord_mat2))
+
+
+  assert_that(alpha >= 0 && alpha <=1)
+  assert_that(dthresh > 0)
+
+
+
+  full_nn <- rflann::RadiusSearch(coord_mat1, coord_mat2, radius=dthresh^2, max_neighbour=nnk)
+  weight_mode <- match.arg(weight_mode)
+
+  nels <- sum(sapply(full_nn$indices, length))
+
+
+  triplet <- cross_fspatial_weights(full_nn$indices, full_nn$distances, feature_mat1, feature_mat2,
+                                    nels, sigma, wsigma, alpha, weight_mode == "binary")
+
+  sm <- sparseMatrix(i=triplet[,1], j=triplet[,2], x=triplet[,3], dims=c(nrow(coord_mat1), nrow(coord_mat2)))
+
+  if (normalized) {
+    D <- 1/rowSums(sm)
+    sm <- D * sm
+  }
+
+
+  sm
+
+}
+
+
+
+#' weighted_spatial_adjacency
+#'
+#' @param coord_mat
+#' @param feature_mat
+#' @param wfun
+#' @param wsigma
+#' @param alpha
+#' @param dthresh
+#' @param include_diagonal
+#' @param normalized
+#' @param stochastic
+#' @importFrom assertthat assert_that
+weighted_spatial_adjacency <- function(coord_mat, feature_mat, wfun=NULL, wsigma=.73, alpha=.5,
+                                       nnk=27,
+                                       weight_mode=c("binary", "heat"),
+                                       sigma=1,
+                                       dthresh=sigma*2.5,
+                                       include_diagonal=TRUE,
+                                       normalized=TRUE,
+                                       stochastic=FALSE) {
+
+
+  assert_that(nrow(feature_mat) == nrow(coord_mat))
+  assert_that(alpha >= 0 && alpha <=1)
+  assert_that(dthresh > 0)
+
+  full_nn <- rflann::RadiusSearch(coord_mat, coord_mat, radius=dthresh^2, max_neighbour=nnk)
+  weight_mode <- match.arg(weight_mode)
+  alpha2 <- 1-alpha
+
+  nels <- sum(sapply(full_nn$indices, length))
+  triplet <- fspatial_weights(full_nn$indices, full_nn$distances, feature_mat, nels, sigma, wsigma, alpha, weight_mode == "binary")
+
+  sm <- sparseMatrix(i=triplet[,1], j=triplet[,2], x=triplet[,3], dims=c(nrow(coord_mat), nrow(coord_mat)))
+
+  if (!include_diagonal) {
+    diag(sm) <- rep(0,nrow(sm))
+  }
+
+  if (normalized) {
+    sm <- normalize_adjacency(sm)
+    if (stochastic) {
+      ## make doubly stochastic (row and columns sum to 1)
+      sm <- make_doubly_stochastic(sm)
+    }
+  }
+
+
+
+  sm
+
+}
+
+
+
+#' spatial_adjacency
+#'
+#' @importFrom rflann RadiusSearch
+#' @importFrom Matrix sparseMatrix
+#' @export
+spatial_adjacency <- function(coord_mat, dthresh=1.42, nnk=27, weight_mode=c("binary", "heat"), sigma=dthresh/2,
+                              include_diagonal=TRUE, normalized=TRUE, stochastic=FALSE) {
+
+  cross_spatial_adjacency(coord_mat, coord_mat, dthresh=dthresh, weight_mode=weight_mode, sigma=sigma,
+                          include_diagonal=include_diagonal, normalized=normalized, stochastic=stochastic)
+
+}
+
+#' cross_spatial_adjacency
+#'
+#' @importFrom rflann RadiusSearch
+#' @importFrom Matrix sparseMatrix
+#' @export
+cross_spatial_adjacency <- function(coord_mat1, coord_mat2, dthresh=1.42, nnk=27, weight_mode=c("binary", "heat"), sigma=dthresh/2,
+                              include_diagonal=TRUE, normalized=TRUE, stochastic=FALSE) {
+
+
+  full_nn <- rflann::RadiusSearch(coord_mat1, coord_mat2, radius=dthresh^2, max_neighbour=nnk)
+  weight_mode <- match.arg(weight_mode)
+
+  nels <- sum(sapply(full_nn$indices, length))
+
+  triplet <- spatial_weights(full_nn$indices, full_nn$distances, nels, sigma, weight_mode == "binary")
+
+  sm <- sparseMatrix(i=triplet[,1], j=triplet[,2], x=triplet[,3], dims=c(nrow(coord_mat1), nrow(coord_mat2)))
+
+  if (!include_diagonal) {
+    diag(sm) <- rep(0,nrow(sm))
+  }
+
+  if (normalized) {
+    sm <- normalize_adjacency(sm)
+    if (stochastic) {
+      ## make doubly stochastic (row and columns sum to 1)
+      sm <- make_doubly_stochastic(sm)
+    }
+  }
+
+
+  sm
+
+}
+
+
+
+
+#' #' spatial_adjacency
+#' #'
+#' #' @importFrom FNN get.knnx
+#' #' @importFrom Matrix sparseMatrix
+#' #' @export
+#' spatial_adjacency <- function(coord_mat, dthresh=1.42, nnk=27) {
+#'   full_nn <- FNN::get.knnx(coord_mat, coord_mat, nnk)
+#'
+#'   triplet <- do.call(rbind, lapply(1:nrow(coord_mat), function(i) {
+#'     dvals <- full_nn$nn.dist[i,1:nnk]
+#'     keep <- dvals > 0 & dvals < dthresh
+#'     if (keep[2]) {
+#'       cbind(i,full_nn$nn.index[i,keep], 1)
+#'     } else {
+#'       NULL
+#'     }
+#'   }))
+#'
+#'   sparseMatrix(i=triplet[,1], j=triplet[,2], x=triplet[,3])
+#'
+#' }
---
+++ src/weight_funs.cpp
@@ -0,0 +1,157 @@
+// [[Rcpp::plugins(cpp11)]]
+#include <Rcpp.h>
+#include <unordered_set>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+//
+//double heat_kernel(NumericVector x1, NumericVector x2, double sigma) {
+//  double dist = sqrt(sum(pow((x1-x2),2)));
+//  return exp(-dist / (2*(pow(sigma,2))));
+//}
+
+
+double norm_heat_kernel(NumericVector x1, NumericVector x2, double sigma) {
+  double norm_dist = sum(pow((x1-x2),2))/(2*x1.length());
+  return exp(-norm_dist/(2*pow(sigma,2)));
+}
+
+
+
+NumericVector distance_heat(NumericVector dist, double sigma) {
+  return exp(-dist/(2*pow(sigma,2)));
+}
+
+
+
+NumericVector normalized_distance(NumericVector dist, double sigma, int len) {
+  NumericVector norm_dist = pow(dist,2)/(2*len);
+  return exp(-norm_dist/(2*pow(sigma,2)));
+}
+
+// [[Rcpp::export]]
+NumericMatrix cross_fspatial_weights(List indices, List distances, NumericMatrix feature_mat1,
+                                     NumericMatrix feature_mat2,
+                                     double nels, double sigma, double fsigma, double alpha, bool binary) {
+  int n = indices.length();
+  List out(n);
+
+  NumericMatrix wout(nels, 3);
+
+  int count = 0;
+
+  double alpha2 = 1 - alpha;
+
+  for(int i = 0; i < n; ++i) {
+    SEXP ll = indices[i];
+    IntegerVector ind(ll);
+
+    if (ind.size() > 0) {
+      NumericVector vals;
+      if (binary) {
+        vals = rep(1.0, ind.size());
+      } else {
+        vals = distance_heat(distances[i], sigma);
+      }
+
+      NumericMatrix::Row f1 = feature_mat1(i,_);
+
+      for (int j=0; j<vals.length(); j++) {
+        NumericMatrix::Row f2 = feature_mat2(ind[j]-1,_);
+        double s = norm_heat_kernel(f1, f2, fsigma);
+        wout(count, 0) = i+1;
+        wout(count, 1) = ind[j];
+        wout(count, 2) = alpha*vals[j] + alpha2*s;
+        count++;
+      }
+    }
+  }
+
+  return wout;
+
+}
+
+
+// [[Rcpp::export]]
+NumericMatrix fspatial_weights(List indices, List distances, NumericMatrix feature_mat,
+                               double nels, double sigma, double fsigma, double alpha, bool binary) {
+  int n = indices.length();
+  List out(n);
+
+  NumericMatrix wout(nels, 3);
+
+  int count = 0;
+
+  double alpha2 = 1 - alpha;
+
+  for(int i = 0; i < n; ++i) {
+    SEXP ll = indices[i];
+    IntegerVector ind(ll);
+
+    if (ind.size() > 0) {
+      NumericVector vals;
+      if (binary) {
+        vals = rep(1.0, ind.size());
+      } else {
+        vals = distance_heat(distances[i], sigma);
+      }
+
+      NumericMatrix::Row f1 = feature_mat(i,_);
+
+      for (int j=0; j<vals.length(); j++) {
+        NumericMatrix::Row f2 = feature_mat(ind[j]-1,_);
+        double s = norm_heat_kernel(f1, f2, fsigma);
+        wout(count, 0) = i+1;
+        wout(count, 1) = ind[j];
+        wout(count, 2) = alpha*vals[j] + alpha2*s;
+        count++;
+      }
+    }
+  }
+
+  return wout;
+
+}
+
+// [[Rcpp::export]]
+NumericMatrix spatial_weights(List indices, List distances, double nels, double sigma, bool binary) {
+  int n = indices.length();
+  List out(n);
+
+  NumericMatrix wout(nels, 3);
+
+  int count = 0;
+
+  for(int i = 0; i < n; ++i) {
+    SEXP ll = indices[i];
+    IntegerVector ind(ll);
+
+    if (ind.size() > 0) {
+      NumericVector vals;
+      if (binary) {
+        vals = rep(1.0, ind.size());
+      } else {
+        vals = distance_heat(distances[i], sigma);
+      }
+
+      for (int j=0; j<ind.length(); j++) {
+          wout(count, 0) = i+1;
+          wout(count, 1) = ind[j];
+          wout(count, 2) = vals[j];
+          count++;
+      }
+    }
+  }
+
+  return wout;
+}
+
