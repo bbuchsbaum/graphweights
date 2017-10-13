0168fc0ffac1f86f635abe3bc9120b23d03ab13e
Bradley Buchsbaum <bbuchsbaum@Donacobius.local>
2017-10-13 12:15:44 -0400

    psparse2 renamed to psparse

--- R/knn_weights.R
+++ R/knn_weights.R
@@ -113,9 +113,9 @@ sim_from_adj <- function(A, k=5, type=c("normal", "mutual"), ncores=1) {
   m <- sparseMatrix(i=A2[,1], j=A2[,2], x=A2[,3], dims=c(nrow(A), nrow(A)))
 
   m <- if (type == "normal") {
-    psparse2(m, pmax)
+    psparse(m, pmax)
   } else {
-    psparse2(m, pmin)
+    psparse(m, pmin)
   }
 }
 
@@ -144,13 +144,13 @@ weighted_knn <- function(X, k=5, FUN=heat_kernel, type=c("normal", "mutual"), re
   W <- indices_to_sparse(nn$nn.index, hval)
 
   if (type == "normal") {
-    psparse2(W, pmax, return_triplet=return_triplet)
+    psparse(W, pmax, return_triplet=return_triplet)
   } else {
-    psparse2(W, pmin, return_triplet=return_triplet)
+    psparse(W, pmin, return_triplet=return_triplet)
   }
 }
 
-psparse2 <- function(M, FUN, return_triplet=FALSE) {
+psparse <- function(M, FUN, return_triplet=FALSE) {
   ind <- which(M != 0, arr.ind=TRUE)
   x1 <- M[ind]
   x2 <- M[cbind(ind[,2], ind[,1])]
@@ -159,45 +159,43 @@ psparse2 <- function(M, FUN, return_triplet=FALSE) {
     cbind(i=c(ind[,1],ind[,2]), j=c(ind[,2],ind[,1]), x=rep(FUN(x1,x2),2))
   } else {
     sm <- sparseMatrix(i=c(ind[,1],ind[,2]), j=c(ind[,2],ind[,1]), x=rep(FUN(x1,x2),2), dims=dim(M), use.last.ij=TRUE)
-    #sm <- sparseMatrix(i=ind[,1], j=ind[,2], x=FUN(x1,x2), dims=dim(W), symmetric=TRUE)
-    #as(sm, "dgTMatrix")
     sm
   }
 }
 
 
 
-psparse <- function(..., fun=c("max", "min"), na.rm=FALSE, return_triplet=FALSE) {
-  fun <- match.arg(fun)
-  # check that all matrices have conforming sizes
-  num.rows <- unique(sapply(list(...), nrow))
-  num.cols <- unique(sapply(list(...), ncol))
-  stopifnot(length(num.rows) == 1)
-  stopifnot(length(num.cols) == 1)
-
-  cat.summary <- do.call(rbind, lapply(list(...), summary))
-
-
-  out.summary <- if (fun == "min") {
-    aggregate(x ~ i + j, data = cat.summary, FUN=function(x) {
-      if (length(x) == 1) 0 else x[1]
-    })
-  } else {
-    aggregate(x ~ i + j, data = cat.summary, FUN=max, na.rm)
-  }
-
-  if (return_triplet) {
-    cbind(i=out.summary$i, j=out.summary$j, x=out.summary$x)
-  } else {
-    sparseMatrix(i = out.summary$i,
-                 j = out.summary$j,
-                 x = out.summary$x,
-                 dims = c(num.rows, num.cols))
-  }
-}
-
-pmin.sparse <- function(..., na.rm = FALSE, return_triplet=FALSE) { psparse(..., fun="min", return_triplet=return_triplet) }
-
-pmax.sparse <- function(..., na.rm = FALSE, return_triplet=FALSE) { psparse(..., fun="max", return_triplet=return_triplet ) }
-
+# psparse <- function(..., fun=c("max", "min"), na.rm=FALSE, return_triplet=FALSE) {
+#   fun <- match.arg(fun)
+#   # check that all matrices have conforming sizes
+#   num.rows <- unique(sapply(list(...), nrow))
+#   num.cols <- unique(sapply(list(...), ncol))
+#   stopifnot(length(num.rows) == 1)
+#   stopifnot(length(num.cols) == 1)
+#
+#   cat.summary <- do.call(rbind, lapply(list(...), summary))
+#
+#
+#   out.summary <- if (fun == "min") {
+#     aggregate(x ~ i + j, data = cat.summary, FUN=function(x) {
+#       if (length(x) == 1) 0 else x[1]
+#     })
+#   } else {
+#     aggregate(x ~ i + j, data = cat.summary, FUN=max, na.rm)
+#   }
+#
+#   if (return_triplet) {
+#     cbind(i=out.summary$i, j=out.summary$j, x=out.summary$x)
+#   } else {
+#     sparseMatrix(i = out.summary$i,
+#                  j = out.summary$j,
+#                  x = out.summary$x,
+#                  dims = c(num.rows, num.cols))
+#   }
+# }
+#
+# pmin.sparse <- function(..., na.rm = FALSE, return_triplet=FALSE) { psparse(..., fun="min", return_triplet=return_triplet) }
+#
+# pmax.sparse <- function(..., na.rm = FALSE, return_triplet=FALSE) { psparse(..., fun="max", return_triplet=return_triplet ) }
+#
 
